8f0c31ff04245c0f35c7fa3ca3cd04f0c71ae152
threadexception <hannes.gaumann@outlook.de>
2024-02-17 01:53:10 +0100

    Fix crops stuff

--- src/de/terrarier/smp/listeners/ListenerCrops.java
+++ src/de/terrarier/smp/listeners/ListenerCrops.java
@@ -1,12 +1,19 @@
 package de.terrarier.smp.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.CropState;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.Crops;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Collection;
 
 public class ListenerCrops implements Listener {
 
@@ -17,23 +24,24 @@ public class ListenerCrops implements Listener {
     }
 
     @EventHandler
-    public void onCrop(BlockBreakEvent ev) {
-        if ((ev.getBlock().getType() == Material.CROPS || ev.getBlock().getType() == Material.CARROT || ev.getBlock().getType() == Material.POTATO)
-                && ev.getPlayer().getItemInHand() != null
-                && ev.getPlayer().getItemInHand().getType().name().contains("HOE")) {
-            for (ItemStack item : ev.getBlock().getDrops()) {
-                if (item.getType() == Material.WHEAT || item.getType() == Material.CARROT_ITEM || item.getType() == Material.POTATO_ITEM) {
-                    // ev.getBlock().setData((byte) 0);
-                    Material prev = ev.getBlock().getType();
-                    ev.getBlock().setType(Material.AIR);
-                    Bukkit.getScheduler().scheduleSyncDelayedTask(instance, () -> ev.getBlock().setType(prev), 1L);
+    public void onCrop(PlayerInteractEvent ev) {
+        if (ev.getAction() == Action.RIGHT_CLICK_BLOCK && ev.getClickedBlock() != null && (ev.getClickedBlock().getType() == Material.POTATO || ev.getClickedBlock().getType() == Material.CARROT || ev.getClickedBlock().getType() == Material.CROPS || ev.getClickedBlock().getType().name().equals("BEETROOT_BLOCK"))
+                && (ev.getPlayer().getItemInHand() == null || ev.getPlayer().getItemInHand().getType() == Material.AIR) && ((Crops) ev.getClickedBlock().getState().getData()).getState() == CropState.RIPE) {
+            for (ItemStack item : ev.getClickedBlock().getDrops()) {
+                if (item.getType() == Material.SEEDS || item.getType() == Material.CARROT_ITEM || item.getType() == Material.POTATO_ITEM) {
                     item.setAmount(item.getAmount() - 1);
                     if (item.getAmount() == 0) {
                         item.setType(Material.AIR);
                     }
-                    return;
                 }
             }
+            Location loc = ev.getClickedBlock().getLocation().clone().add(0.5, 0, 0.5);
+            for (ItemStack drop : ev.getClickedBlock().getDrops()) {
+                loc.getWorld().dropItem(loc, drop);
+            }
+            Material prev = ev.getClickedBlock().getType();
+            ev.getClickedBlock().setType(Material.AIR);
+            ev.getClickedBlock().setType(prev);
         }
     }
 
